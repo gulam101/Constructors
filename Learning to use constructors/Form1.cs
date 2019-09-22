8a3af32f92126e7aed46fba6440787c62db7fcd5
unknown <gfram@hotmail.com>
2019-09-22 14:34:20 +0100

    first commit

---
+++ Learning to use constructors/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Learning_to_use_constructors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool redHair = false;
+            MessageBox.Show(redHair.ToString());
+        }
+    }
+}
